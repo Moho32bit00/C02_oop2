2d6b66e8f2acb8c6b54232ff90c17385d98411c7
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-16 19:10:37 +0300

    Part 01 : Theoretical Questions Question 1

--- oop02_assi/Program.cs
+++ oop02_assi/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-
+            //Part 01 : Theoretical Questions
+            //Question 1
+            //a) class support inhertance while struct doesn't
+            //b) because class do support all oop pillars and more suitable for complex data and operations
         }
     }
 }
