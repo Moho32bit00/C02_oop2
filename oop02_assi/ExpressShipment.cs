ca7f2e027f6333e052f70c0f48e1193f2797d5f4
Moho32bit00 <mohamed.man2007@gmail.com>
2026-09-16 19:29:38 +0300

    2. Create Three Shipment Types

---
+++ oop02_assi/ExpressShipment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C02_oop02
+{
+    public class ExpressShipment : Shipment
+    {
+        public decimal ExtraFee
+        {
+            get { return ExtraFee; }
+            set
+            {
+                if (ExtraFee >= 0)
+                {
+                    ExtraFee = value;
+                }
+            }
+        }
+        public override decimal EstimatedCost { get { return DeliveryFee + (Weight * 5) + ExtraFee; } }
+
+        public ExpressShipment() { }
+        public ExpressShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination, decimal fee) : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            Destination = destination;
+            this.ExtraFee = ExtraFee;
+        }
+
+    }
+}
---
+++ oop02_assi/InternationalShipment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C02_oop02
+{
+    public class InternationalShipment : Shipment
+    {
+        public string DestinationCountry
+        {
+            get { return DestinationCountry; }
+            set
+            {
+                if (DestinationCountry != null || DestinationCountry != "" || DestinationCountry != " ")
+                {
+                    DestinationCountry = value;
+                }
+            }
+        }
+        public decimal CustomsFee
+        {
+            get { return CustomsFee; }
+            set
+            {
+                if (CustomsFee >= 0)
+                {
+                    CustomsFee = value;
+                }
+            }
+
+        }
+
+        public override decimal EstimatedCost
+        {
+            get { return DeliveryFee + (Weight * 5) + CustomsFee; }
+        }
+        public InternationalShipment() { }
+        public InternationalShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination, string DestinationCountry, decimal CustomsFee) : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            this.CustomsFee = CustomsFee;
+            this.DestinationCountry = DestinationCountry;
+        }
+    }
+}
